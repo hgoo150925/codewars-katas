549dc9f2a764966b9c84812d2a20cdc3b37c3b80
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-24 15:02:42 -0300

    Create Phone Number

--- 6-kyu/Create Phone Number/index.js
+++ 6-kyu/Create Phone Number/index.js
@@ -1,5 +1,7 @@
-function createPhoneNumber(numbers) {
-    return numbers.toString().split(' ').join('')
+const createPhoneNumber = (numbers) => {
+    // let phoneNumber = numbers.join('').replace(/,/g, "");
+    let phoneNumber = numbers.toString().replace(/,/g, "");
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`
 }
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
-module.exports = createPhoneNumber
+
+module.exports = createPhoneNumber;
