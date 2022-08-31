2df2f9ade2b7e08073b6d0d536001b5393ce512f
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-31 15:44:14 -0300

    Multiples of 3 or 5

---
+++ 6-kyu/Multiples of 3 or 5/index.js
@@ -0,0 +1,12 @@
+const solution = num => {
+  let sum = 0;
+
+  for (let i = 1; i < num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+module.exports = solution;
---
+++ 6-kyu/Multiples of 3 or 5/index.test.js
@@ -0,0 +1,8 @@
+const solution = require('.');
+
+describe('Testing...', function () {
+  test('Test 1', () => {
+    let result = 23;
+    expect(solution(10)).toBe(result);
+  });
+});
