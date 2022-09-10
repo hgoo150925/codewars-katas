c43769451626b27d1de50c925b5f7219fa5bd3b1
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-10 13:51:30 -0300

    Replace with Alphabet Position

--- 6-kyu/Replace With Alphabet Position/index.js
+++ 6-kyu/Replace With Alphabet Position/index.js
@@ -17,3 +17,14 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 Kata Link: https://www.codewars.com/kata/replace-with-alphabet-position
 */
+
+const alphabetPosition = text => {
+  // Recibo un string
+  // Debo reemplazar cada letra del string por su posicion en el alfabeto
+  // Recibo un string de letras y retorno un string de numeros
+  const words = text.toLowerCase().replace(/[^a-z]/g, '');
+  // charCodeAt ->  obtener el código de carácter ASCII de cada carácter y le restamos 96 para tener la posicion en el alfabeto
+  return [...words].map(v => v.charCodeAt() - 96).join(' ');
+};
+
+module.exports = alphabetPosition;
